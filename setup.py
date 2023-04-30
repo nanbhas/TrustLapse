b0a598348579506b4ec0cd20f1790a11a1cc5f8d
Nandita Bhaskhar <nanbhas@stanford.edu>
2023-04-29 22:36:42 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="trustlapse",
+    version="0.0.1",
+    description="TrustLapse: An Explainable and Actionable Mistrust Scoring Framework for Model Monitoring",
+    author="Nandita Bhaskhar",
+    author_email="nanbhas@stanford.edu",
+    url="https://github.com/nanbhas/TrustLapse",
+    install_requires=[""],
+    packages=find_packages(),
+)
